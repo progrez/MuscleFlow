bb9761a7f1509fcf3473e58ac07517c330f4f69c
KseniiaMasna <kseniia.masna@gmail.com>
2025-01-14 09:24:04 +0100

    Develope login page

---
+++ profile-page/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginScreen from './pages/LoginScreen'
+
+function App() {
+
+  return (
+    <>
+      <LoginScreen />
+    </>
+  )
+}
+
+export default App
---
+++ profile-page/src/pages/LoginScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+const LoginScreen = () => {
+    return(
+        <>
+            <h1>Log in</h1>
+            <LoginForm />
+            <a href="">Sign Up</a>
+        </>
+        
+    )
+}
+
+ export default LoginScreen 
