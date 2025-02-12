00455df20a753e315aae6ece4228b84966eba87b
Pawulina <paulinawalcz94@gmail.com>
2025-02-12 11:37:19 +0100

    Toggle menu if logged in, otherwise navigate to /login. Changed tailwind class width from w-full in component, to have possibility to setup it directly in the place where u put it. Added log out ffunction.

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
             <button 
                 onClick={props.onClick}
                 type={props.type}
-                className={`flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-neutraLight shadow-sm hover:bg-primary-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-focus ${props.className}`}
+                className={`flex justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-neutraLight shadow-sm hover:bg-primary-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-focus ${props.className}`}
             >{props.label}</button>
         </div>
 
